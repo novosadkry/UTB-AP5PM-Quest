52efc9282c02ae2888d930ed6336ab558b584204
Kryštof Novosad <novosadkry@seznam.cz>
2025-12-12 08:48:00 +0100

    chore: initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UTB-AP5PM-Quest',
+  webDir: 'dist'
+};
+
+export default config;
