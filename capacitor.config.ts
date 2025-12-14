c4a35948e714745a7b69dd7474a02630e8ed61e9
Kryštof Novosad <novosadkry@seznam.cz>
2025-12-14 15:46:03 +0100

    fix: @capacitor-firebase config setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'UTB-AP5PM-Quest',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    FirebaseAuthentication: {
+      authDomain: undefined,
+      skipNativeAuth: false,
+      providers: ['google.com']
+    }
+  }
 };
 
 export default config;
